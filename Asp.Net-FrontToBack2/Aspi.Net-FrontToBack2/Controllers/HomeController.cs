c80b7f41ac9baaa91ab6bf3f1f4d0227c1404872
toghrulvg <toghrulvg@code.edu.az>
2022-10-03 14:45:14 -0700

    added

---
+++ Asp.Net-FrontToBack2/Aspi.Net-FrontToBack2/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Aspi.Net_FrontToBack2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aspi.Net_FrontToBack2.Controllers
+{
+    public class HomeController : Controller
+    {     
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
